c43a43993121e2431497f2c9a77cd0de72d65157
dapgo <danielperezglez@users.sourceforge.net>
2018-07-24 12:08:41 +0200

    Add files via upload

---
+++ files_data_format/ffnotes_local_file-storage.js
@@ -0,0 +1,20 @@
+{
+	"theme": "default",
+	"redux": "{\"sync\":{\"welcomePage\":false,\"focusedNoteId\":\"xxxxxxxmaskedxxxxxx\",\"isSyncing\":false,\"lastSynced\":\"2018-07-24T08:28:16.719Z\",\"error\":null,\"isOpeningLogin\":false,\"isPleaseLogin\":false,\"isReconnectSync\":false,\"email\":\"myemail@dapgo.github.io\"},\"kinto\":{\"isLoaded\":true},\"notes\":[{\"id\":\"e236-71f6-4341-92ea-b47faa465\",\"content\":\"<p><strong>My Nota1 by DPG&nbsp;</strong></p><p>testing notes for firefox, in order to be included in project PIM_Data_Interoperability</p><p>&nbsp;</p><ul><li>Tech info</li></ul><p>Data is stored in the profile folder</p><p>browser-extension-data\\\\notes@mozilla.com</p><p>&nbsp;</p><ul><li>my outline1</li><li>my outline2</li></ul>\",\"lastModified\":\"2018-07-24T08:28:14.847Z\",\"_status\":\"synced\",\"last_modified\":1532419761748,\"firstLine\":\"My Nota1 by DPG\",\"secondLine\":\"testing notes for firefox, in order to be included in project PIM_Data_Interoperability   Tech info Data is stored in the profile folder browser-extension-data\\\\notes@mozilla.com   my outline1 my outline2\"},{\"id\":\"initialNote\",\"content\":\"<h2>Welcome to Firefox Notes!</h2><p>A simple tool for taking notes while you browse.</p><p><strong>To open Notes:</strong></p><ul><li>On Windows, Linux: <code>Alt+Shift+W</code></li><li>On Mac: <code>Opt+Shift+W</code></li></ul><p><strong>To access your notes from any laptop or desktop computer:</strong></p><ul><li>Select <strong>Sync Your Notes</strong> to sign into your Firefox Account.</li></ul><p><strong>To format your text</strong>, use the toolbar or these commands:</p><ul><li>Headings: Begin a line with <code>#</code> or <code>##</code> or <code>###</code> followed by a space to create a heading 1, heading 2, or heading 3</li><li>Bold: <code>**text**</code> or <code>__text__</code></li><li>Italics: <code>*text*</code> or <code>_text_</code></li><li>Bulleted list: Begin a line with <code>*</code>  or  <code>–</code>  followed by a space</li><li>Numbered list: Begin a line with <code>1.</code> or <code>1)</code> followed by a space</li><li>Code: <code>`text`</code></li></ul><p><strong>Have a suggestion to improve Notes?</strong></p><ul><li>Select <strong>Give Feedback</strong> in the Notes menu.</li></ul><p><strong>That’s it</strong>—you’re ready to take notes!</p>\",\"lastModified\":\"2018-07-24T07:40:35.706Z\",\"_status\":\"synced\",\"last_modified\":1532419761764,\"firstLine\":\"Welcome to Firefox Notes!\",\"secondLine\":\"A simple tool for taking notes while you browse. To open Notes: On Windows, Linux: Alt+Shift+W On Mac: Opt+Shift+W To access your notes from any laptop or desktop computer: Select Sync Your Notes to sign into your Firefox Account. To format your text\"}]}",
+	"credentials": {
+		"access_token": "xxxxxxxmaskedxxxxxx",
+		"refresh_token": "xxxxxxxmaskedxxxxxx",
+		"key": {
+			"kty": "oct",
+			"scope": "https://identity.mozilla.com/apps/notes",
+			"k": "xxxxxxxmaskedxxxxxx",
+			"kid": "xxxxxxxmaskedxxxxxx"
+		},
+		"metadata": {
+			"server": "https://oauth.accounts.firefox.com/v1",
+			"client_id": "xxxxxxxxyyyxx",
+			"scope": ["profile",
+			"https://identity.mozilla.com/apps/notes"]
+		}
+	}
+}
